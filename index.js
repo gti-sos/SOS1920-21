9156a612c7f1fe70909e8fcd1f3b7b483bcc2f5f
juanillo62gm <juanillo62gm@gmail.com>
2020-03-11 14:45:20 +0000

    Removed spanish comments and moved cool face

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const cool = require("cool-ascii-faces");	// Carga el mรณdulo en la variable cool
+const cool = require("cool-ascii-faces");
 const express = require("express");
 
-var app = express();	// Se construye un objeto
-
+var app = express();
 var port = process.env.PORT || 80;
 
 app.get("/public", (request,response) => {
 	response.sendFile(__dirname + "/public/index.html");
 });
 
-
-app.listen(port,() => { 					// Servidor web listo
+app.listen(port,() => {
 	console.log("Server ready! ");
+	console.log(cool());
 });	
-
-
-console.log("I love js");
-console.log(cool());
