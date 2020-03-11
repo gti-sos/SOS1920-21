c9039ac1e14472160db074c8f67cdb0121f0b8a6
juanillo62gm <juanillo62gm@gmail.com>
2020-03-11 11:11:55 +0000

    Server on /public and with index.html like on SOS1920 repository

---
+++ index.js
@@ -0,0 +1,15 @@
+const cool = require("cool-ascii-faces");	// Carga el mรณdulo en la variable cool
+const express = require("express");
+
+var app = express();	// Se construye un objeto
+
+app.get("/public", (request,response) => {
+	response.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.listen(80);		// Servidor web listo
+console.log("Server ready! ");
+
+console.log("I love js");
+console.log(cool());
