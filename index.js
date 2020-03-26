134b1d98c24127c928a082773409aaf29c13538c
root <antonio98pt@gmail.com>
2020-03-26 10:59:49 +0000

    Parte Antonio F03: terminada

--- index.js
+++ index.js
@@ -838,6 +838,11 @@ app.delete(BASE_API_URL+"/driving_licenses/:aut_com", (req,res)=>{
 	
 });
 
+// Redirect to postman url
+app.get("/api/v1/driving_licenses/docs", (req, res)=>{
+   res.redirect("https://documenter.getpostman.com/view/10660202/SzYUZLpP");
+});
+
 // ==================================================
 // =============== Start juagommat ================= 
 // ==================================================
