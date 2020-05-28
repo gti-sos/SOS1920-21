e60c4c07ab89d0097d802d07d4010e87e1193e28
Juanillo62gm <juangomezm97@gmail.com>
2020-05-28 18:11:21 +0200

    Testing codecov

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -9,13 +9,13 @@
 	import TI from './traffic-injuries/Home.svelte';
 	import EditTI from './traffic-injuries/editTrafficInjury.svelte';
 	import D3JS from './traffic-injuries/d3js.svelte';
-	
+
 	// driving-licenses
 	import DL from './driving-licenses/Home.svelte';
 	import EditDL from './driving-licenses/editDrivingLicense.svelte';
 
 	const routes = {
-		"/": Home ,
+		"/": Home,
 		"/info": Info,
 		"/traffic-injuries": TI,
 		"/traffic-injuries/:auto_com/:year": EditTI,
@@ -27,43 +27,45 @@
 </script>
 
 <body>
-	<!--Navbar-->
-	<nav class="navbar navbar-expand-lg navbar-light primary-color">
+	<header>
+		<!--Navbar-->
+		<nav class="navbar navbar-expand-lg navbar-light primary-color">
 
-		<!-- Navbar brand -->
-		<a class="navbar-brand" href="/">SOS1920-21</a>
-	  
-		<!-- Collapse button -->
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
-		  aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-		  <span class="navbar-toggler-icon"></span>
-		</button>
-	  
-		<!-- Collapsible content -->
-		<div class="collapse navbar-collapse" id="basicExampleNav">
-	  
-		  <!-- Links -->
-		  <ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-			  <a class="nav-link" href="svelte.html#/traffic-injuries">Accidentes de Tráfico</a>
-			</li>
-			<li class="nav-item">
-			  <a class="nav-link" href="svelte.html#/driving-licenses">Licencias de Conducir</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="svelte.html#/info">Información</a>
-			  </li>
-	  
-			<!-- Dropdown -->
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-				aria-haspopup="true" aria-expanded="false">Extras</a>
-			  <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-				<a class="dropdown-item" href="svelte.html#/traffic-injuries/d3js">Accidentes de Tráfico (Librería D3JS) </a>
-			  </div>
-			</li>
-		  </ul>	  
-	  </nav>
+			<!-- Navbar brand -->
+			<a class="navbar-brand" href="/">SOS1920-21</a>
+
+			<!-- Collapse button -->
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+				aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<!-- Collapsible content -->
+			<div class="collapse navbar-collapse" id="basicExampleNav">
+
+				<!-- Links -->
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="svelte.html#/traffic-injuries">Accidentes de Tráfico</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="svelte.html#/driving-licenses">Licencias de Conducir</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="svelte.html#/info">Información</a>
+					</li>
+					<!-- Dropdown -->
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+							aria-haspopup="true" aria-expanded="false">Extras</a>
+						<div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="svelte.html#/traffic-injuries/d3js">Accidentes de Tráfico
+								(Librería D3JS) </a>
+						</div>
+					</li>
+				</ul>
+		</nav>
+	</header>
 
 	<div class="container">
 		<Router {routes} />
