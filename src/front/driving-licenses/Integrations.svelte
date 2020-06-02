f12c51c978c564d559a90a9b50a424ebc8ef754b
unknown <antonio98pt@gmail.com>
2020-06-03 01:23:34 +0200

    Grupo 1 integrado

--- src/front/driving-licenses/Integrations.svelte
+++ src/front/driving-licenses/Integrations.svelte
@@ -1,216 +1,218 @@
 <script>
-    import { Button, Alert } from 'sveltestrap';
+  import { Button, Alert } from "sveltestrap";
 
-    // Alerts
-    let infoAlertStatus = "";
-    let infoAlertText = "";
+  // Alerts
+  let infoAlertStatus = "";
+  let infoAlertText = "";
 
-    // Natality-stats
-    async function loadInitialDataNatalityStats() {
-        const res = await fetch("https://sos1920-01.herokuapp.com/api/v2/natality-stats/loadInitialData", {
-            method: "GET"
-        }).then(function (res) {
-            highchartsGraphG1();
-            infoAlertStatus = res.status + " - " + res.statusText;
-            infoAlertText = "Recursos cargados correctamente.";
-        });
-    }
+  // Natality-stats
+  async function loadInitialDataNatalityStats() {
+    const res = await fetch(
+      "https://sos1920-01.herokuapp.com/api/v2/natality-stats/loadInitialData",
+      {
+        method: "GET"
+      }
+    ).then(function(res) {
+      highchartsGraphG1();
+      infoAlertStatus = res.status + " - " + res.statusText;
+      infoAlertText = "Recursos cargados correctamente.";
+    });
+  }
 
-    async function deleteAllNatalityStats() {
-        const res = await fetch("https://sos1920-01.herokuapp.com/api/v2/natality-stats", {
-            method: "DELETE"
-        }).then(function (res) {
-            if (res.status == 404) {
-                infoAlertStatus = res.status + " - " + res.statusText;
-                infoAlertText = "No hay recursos que eliminar.";
-            } else {
-                highchartsGraphG1();
-                infoAlertStatus = res.status + " - " + res.statusText;
-                infoAlertText = "Se han eliminado todos los recursos correctamente.";
-            }
-        });
-    }
+  async function deleteAllNatalityStats() {
+    const res = await fetch(
+      "https://sos1920-01.herokuapp.com/api/v2/natality-stats",
+      {
+        method: "DELETE"
+      }
+    ).then(function(res) {
+      if (res.status == 404) {
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "No hay recursos que eliminar.";
+      } else {
+        highchartsGraphG1();
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "Se han eliminado todos los recursos correctamente.";
+      }
+    });
+  }
 
-    async function highchartsGraphG1() {
-        console.log("Fetching poverty-stats...");
-        const data = await fetch("https://sos1920-01.herokuapp.com/api/v2/natality-stats");
-        let jsonData = await data.json();
-        console.log(jsonData);
-        var UK = jsonData.filter(function (x) {
-            return x.country == "unitedKingdom" && parseInt(x.year) || x.country == "spain" && parseInt(x.year);
-        }).map((dato) => {
-            return { 'name': dato.country + ' - ' + dato.year, 'data': [parseInt(dato.poverty_ht), parseInt(dato.poverty_prp), parseInt(dato.poverty_pt)] }
-        });
+  async function highchartsGraphG1() {
+    console.log("Fetching poverty-stats...");
+    const data = await fetch(
+      "https://sos1920-01.herokuapp.com/api/v2/natality-stats"
+    );
+    let jsonData = await data.json();
+    console.log(jsonData);
+    var natalityData = jsonData
+      .filter(function(x) {
+        return x.country && parseInt(x.year) == 2010;
+      })
+      .map(dato => {
+        return {
+          name: dato.country,
+          data: [
+            parseInt(dato.natality_men),
+            parseInt(dato.natality_women),
+            parseInt(dato.natality_totals)
+          ]
+        };
+      });
 
-        Highcharts.chart('container1', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Índices de pobreza entre Reino Unido y España'
-            },
-            subtitle: {
-                text: '(2017 - 2010)'
-            },
-            xAxis: {
-                categories: ["Riesgo de Pobreza", "Umbral Persona", "Umbral Hogar"],
-                crosshair: true
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Miles'
-                }
-            },
-            tooltip: {
-                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-                footerFormat: '</table>',
-                shared: true,
-                useHTML: true
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            series: UK
-        });
-    }
-    highchartsGraphG1();
+    Highcharts.chart("container1", {
+      chart: {
+        type: "column"
+      },
+      title: {
+        text: "Natalidad por sexos"
+      },
+      subtitle: {
+        text: "(2010)"
+      },
+      xAxis: {
+        categories: ["Niños", "Niñas", "Total"],
+        crosshair: true
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Número de niños nacidos"
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat:
+          '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+        footerFormat: "</table>",
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: natalityData
+    });
+  }
+  highchartsGraphG1();
 
-    
+  async function highchartsGraphG2() {
+    console.log("Fetching Natality-stats...");
+    const data = await fetch(
+      "https://sos1920-01.herokuapp.com/api/v2/natality-stats"
+    );
+    let jsonData = await data.json();
+    console.log(jsonData);
+    var natalityData = jsonData
+      .filter(function(x) {
+        return x.country && parseInt(x.year) == 2010;
+      })
+      .map(dato => {
+        return {
+          name: dato.country,
+          y: [
+            parseInt(dato.natality_men),
+            parseInt(dato.natality_women),
+            parseInt(dato.natality_totals)
+          ]
+        };
+      });
+
+    Highcharts.chart("container2", {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: "pie"
+      },
+      title: {
+        text: "Natalidad por sexos"
+      },
+      subtitle: {
+        text: "(2010)"
+      },
+      tooltip: {
+        pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
+      },
+      accessibility: {
+        point: {
+          valueSuffix: "%"
+        }
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true
+        }
+      },
+      series: natalityData
+    });
+  }
+  highchartsGraphG2();
 </script>
 
 <main>
-    <h2>Integraciones - Accidentes de Tráfico</h2>
-    <br>
-    <div class="row">
-        <div class="col-4">
-            <div class="list-group" id="list-tab" role="tablist">
+  <h2>Integraciones - Accidentes de Tráfico</h2>
+  <br />
+  <div class="row">
+    <div class="col-4">
+      <div class="list-group" id="list-tab" role="tablist">
 
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list"
-                    href="#list-01" role="tab" aria-controls="home">Integración con Grupo 1 <br>(Natality-stats)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-02" role="tab" aria-controls="profile">Integración con Grupo 2
-                    <br>(rural-tourism-stats)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-05" role="tab" aria-controls="profile">Integración con Grupo 5 <br>(books-exports)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-09" role="tab" aria-controls="profile">Integración con Grupo 9
-                    <br>(oil-coal-nuclear-energy-consumption-stats)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-10" role="tab" aria-controls="profile">Integración con Grupo 10
-                    <br>(global-marriages)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-22" role="tab" aria-controls="profile">Integración con Grupo 22
-                    <br>(og-basket-stats)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-24" role="tab" aria-controls="profile">Integración con Grupo 24 <br>(univregs-stats)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-26" role="tab" aria-controls="profile">Integración con Grupo 26 <br>(goalscorers)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-28" role="tab" aria-controls="profile">Integración con Grupo 28 <br>(ppas)</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list"
-                    href="#list-30" role="tab" aria-controls="profile">Integración con Grupo 30
-                    <br>(indice_de_masa_corporal)</a>
-            </div>
-        </div>
-        <div class="col-8">
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-01" role="tabpanel" aria-labelledby="list-home-list">
-                    <h2>Integración con Grupo 1 (poverty-stats)</h2>
-                    <h3>Acciones</h3>
-                    {#if infoAlertStatus}
-                    <Alert>
-                        <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
-                        {infoAlertText}
-                    </Alert>
-                    {/if}
-                    <p><a href="/"><Button color="info">Volver a Inicio</Button></a></p>
-                    <p><Button color="success" on:click="{loadInitialDataNatalityStats}">Cargar Datos Iniciales</Button></p>
-                    <p><Button color="danger" on:click="{deleteAllNatalityStats}">Elimina Todos los Recursos</Button></p>
-                    <figure class="highcharts-figure">
-                        <div id="container1"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-02" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 2 (rural-tourism-stats)</h2>
-                    <h3>Acciones</h3>
-                    {#if infoAlertStatus}
-                    <Alert>
-                        <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
-                        {infoAlertText}
-                    </Alert>
-                    {/if}
-                    <p><a href="/"><Button color="info">Volver a Inicio</Button></a></p>
-                    <p><Button color="success" on:click="{loadInitialDataRuralTourismStats}">Cargar Datos Iniciales</Button></p>
-                    <p><Button color="danger" on:click="{deleteAllRuralTourismStats}">Elimina Todos los Recursos</Button></p>
-                    <figure class="highcharts-figure">
-                        <div id="container2"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-05" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 5 (books-exports)</h2>
-                    <h3>Acciones</h3>
-                    {#if infoAlertStatus}
-                    <Alert>
-                        <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
-                        {infoAlertText}
-                    </Alert>
-                    {/if}
-                    <p><a href="/"><Button color="info">Volver a Inicio</Button></a></p>
-                    <p><Button color="success" on:click="{loadInitialDataBooksExports}">Cargar Datos Iniciales</Button></p>
-                    <p><Button color="danger" on:click="{deleteAllBooksExports}">Elimina Todos los Recursos</Button></p>
-                    <figure class="highcharts-figure">
-                        <div id="container5"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-09" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 9 (oil-coal-nuclear-energy-consumption-stats)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container9"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-10" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 10 (global-marriages)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container10"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-22" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 22 (og-basket-stats)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container22"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-24" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 24 (univregs-stats)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container24"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-26" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 26 (goalscorers)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container26"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-28" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 28 (ppas)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container28"></div>
-                    </figure>
-                </div>
-                <div class="tab-pane fade" id="list-30" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h2>Integración con Grupo 30 (indice_de_masa_corporal)</h2>
-                    <figure class="highcharts-figure">
-                        <div id="container30"></div>
-                    </figure>
-                </div>
-            </div>
-        </div>
+        <a
+          class="list-group-item list-group-item-action active"
+          id="list-home-list"
+          data-toggle="list"
+          href="#list-01"
+          role="tab"
+          aria-controls="home">
+          Integración con Grupo 1
+          <br />
+          (Natality-stats)
+        </a>
+      </div>
     </div>
+    <div class="col-8">
+      <div class="tab-content" id="nav-tabContent">
+        <div
+          class="tab-pane fade show active"
+          id="list-01"
+          role="tabpanel"
+          aria-labelledby="list-home-list">
+          <h2>Integración con Grupo 1 (Natality-stats)</h2>
+          <h3>Acciones</h3>
+          {#if infoAlertStatus}
+            <Alert>
+              <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
+              {infoAlertText}
+            </Alert>
+          {/if}
+          <p>
+            <a href="/">
+              <Button color="info">Volver a Inicio</Button>
+            </a>
+          </p>
+          <p>
+            <Button color="success" on:click={loadInitialDataNatalityStats}>
+              Cargar Datos Iniciales
+            </Button>
+          </p>
+          <p>
+            <Button color="danger" on:click={deleteAllNatalityStats}>
+              Elimina Todos los Recursos
+            </Button>
+          </p>
+          <figure class="highcharts-figure">
+            <div id="container1" />
+          </figure>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
