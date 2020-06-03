5a961a10d1f81b7391d76daec121c2678b28733c
Juanillo62gm <juangomezm97@gmail.com>
2020-06-03 22:51:49 +0200

    Fully working team integration

--- src/front/Analytics.svelte
+++ src/front/Analytics.svelte
@@ -5,12 +5,12 @@
     let infoAlertStatus = "";
     let infoAlertText = "";
 
-    async function loadInitialData(){
+    async function loadInitialData() {
         loadInitialDataTrafficInjuries();
         loadInitialDataDrivingLicenses();
     }
 
-    async function deleteAllData(){
+    async function deleteAllData() {
         deleteAllTrafficInjuries();
         deleteAllDrivingLicenses();
     }
@@ -69,17 +69,17 @@
         console.log("Fetching traffic-injuries...");
         const data1 = await fetch("/api/v2/traffic-injuries");
         let jsonData1 = await data1.json();
-        console.log(jsonData1);
 
         console.log("Fetching driving-licenses...");
         const data2 = await fetch("/api/v2/driving-licenses");
         let jsonData2 = await data2.json();
-        console.log(jsonData2);
 
-        var higchartsData = jsonData1.filter(function (x) {
-            return x.auto_com && parseInt(x.year);
+        const res1 = Object.assign(jsonData1, jsonData2)
+
+        var higchartsData = res1.filter(function (x) {
+            return (x.auto_com || x.aut_com) && parseInt(x.year);
         }).map((dato) => {
-            return { 'name': dato.auto_com + ' - ' + dato.year, 'data': [parseInt(dato.accident), parseInt(dato.dead), parseInt(dato.injure)] }
+            return { 'name': (dato.auto_com || dato.aut_com) + " - " + dato.year, 'data': [parseInt(dato.accident), parseInt(dato.dead), parseInt(dato.injure), parseInt(dato.total_mot), parseInt(dato.total_cars)]}
         });
 
         Highcharts.chart('chart', {
@@ -87,15 +87,18 @@
                 type: 'bar'
             },
             title: {
-                text: 'Incidentes de tráfico producidos en España entre 2015 y 2018'
+                text: 'Integración entre accidentes de tráfico y vehículos implicados'
+            },
+            subtitle: {
+                text: '(2015 - 2018)'
             },
             xAxis: {
-                categories: ['Accidentes', 'Muertes', 'Heridos']
+                categories: ['Accidentes', 'Muertes', 'Heridos', 'Total Motos', 'Total Coches']
             },
             yAxis: {
                 min: 0,
                 title: {
-                    text: 'Total'
+                    text: 'Total (miles)'
                 }
             },
             legend: {
@@ -125,8 +128,7 @@
     <p><a href="/"><Button color="info">Volver</Button></a></p>
     <p><Button color="success" on:click="{loadInitialData}">Cargar Datos Iniciales</Button></p>
     <p><Button color="danger" on:click="{deleteAllData}">Elimina Todos los Recursos</Button></p>
-  
-    <br><figure>
+    <figure>
       <div id="chart"></div>
     </figure>
   </main>
--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -1,6 +1,7 @@
 <body>
 	<!-- Masthead-->
-	<br><br><header class="masthead">
+	<br><br>
+	<header class="masthead">
 		<div class="container h-100">
 			<div class="row h-100 align-items-center justify-content-center text-center">
 				<div class="col-lg-10 align-self-end">
@@ -8,15 +9,16 @@
 					<hr class="divider my-4" />
 				</div>
 				<div class="col-lg-8 align-self-baseline">
-					<p class="text-white-75 font-weight-light mb-5">Nuestra fuente de información está orientada a
+					<a href="https://github.com/gti-sos/SOS1920-21"><button type="button" class="btn btn-primary">Repositorio de GitHub</button></a>
+					<br><br><p class="text-white-75 font-weight-light mb-5">Nuestra fuente de información está orientada a
 						analizar la relación entre las víctimas en accidentes de tráfico y permisos de conducir en
 						España.</p>
-
 				</div>
+				
 			</div>
 		</div>
 	</header>
-	<!-- Acerca de Nosotros-->
+	<!-- Información -->
 	<section class="page-section" id="informacion">
 		<div class="jumbotron bg-primary">
 			<div class="container">
@@ -99,9 +101,38 @@
 			<div>
 	</section>
 
+	<!-- Videos -->
+	<section class="page-section" id="equipo">
+		<div class="jumbotron bg-primary">
+			<div class="container">
+				<h2 class="text-center text-white mt-0">Videos</h2>
+				<hr class="divider light my-4" />
+				<div class="row">
+					<div class="col-lg-3 col-md-6 text-center"></div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="mt-5">
+							<i class="fas fa-4x fab fa-youtube text-white mb-4"></i>
+							<h3 class="h4 mb-2 text-white">Juan Gómez Mateos</h3>
+							<p class="text-white-50 mb-0">Accidentes de Tráfico</p>
+							<br><a href=""><button type="button" class="btn btn-light">Ver</button></a>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="mt-5">
+							<i class="fas fa-4x fab fa-youtube text-white mb-4"></i>
+							<h3 class="h4 mb-2 text-white">Antonio Prieto Tagua</h3>
+							<p class="text-white-50 mb-0 ">Licencias de Conducir</p>
+							<br><a href=""><button type="button" class="btn btn-light">Ver</button></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
 	<!-- Equipo-->
 	<section class="page-section" id="equipo">
-		<div class="jumbotron bg">
+		<div class="jumbotron ">
 			<div class="container">
 				<h2 class="text-center text-dark mt-0">Equipo</h2>
 				<hr class="divider light my-4" />
