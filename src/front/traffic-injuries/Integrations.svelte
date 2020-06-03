b4a2e15b18bcbda8527e3f7c9b22c2db3894f1ab
Juanillo62gm <juangomezm97@gmail.com>
2020-06-03 23:33:50 +0200

    Fully working integrations

--- src/front/traffic-injuries/Integrations.svelte
+++ src/front/traffic-injuries/Integrations.svelte
@@ -325,7 +325,7 @@
             tooltip: {
                 headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                    '<td style="padding:0"><b>{point.y:.1f} miles</b></td></tr>',
                 footerFormat: '</table>',
                 shared: true,
                 useHTML: true
@@ -429,7 +429,7 @@
             },
             tooltip: {
                 split: true,
-                valueSuffix: ' millions'
+                valueSuffix: ' miles'
             },
             plotOptions: {
                 area: {
@@ -1010,9 +1010,11 @@
                     <p><a href="https://sos1920-01.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataPovertyStats}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllPovertyStats}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container1"></div>
                     </figure>
+                </div>
                 </div>
                 <div class="tab-pane fade" id="list-02" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 2 (rural-tourism-stats)</h2>
@@ -1027,9 +1029,11 @@
                     <p><a href="https://sos1920-02.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataRuralTourismStats}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllRuralTourismStats}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container2"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-05" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 5 (books-exports)</h2>
@@ -1044,9 +1048,11 @@
                     <p><a href="https://sos1920-05.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataBooksExports}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllBooksExports}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container5"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-09" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 9 (oil-coal-nuclear-energy-consumption-stats)</h2>
@@ -1061,9 +1067,11 @@
                     <p><a href="https://sos1920-09.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataRenovableEnergy}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllRenovableEnergy}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container9"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-10" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 10 (global-marriages)</h2>
@@ -1078,9 +1086,11 @@
                     <p><a href="https://sos1920-10.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataGlobalMarriages}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllGlobalMarriages}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container10"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-22" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 22 (og-basket-stats)</h2>
@@ -1095,9 +1105,11 @@
                     <p><a href="https://sos1920-22.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataOgBasketStats}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllOgBasketStats}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container22"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-24" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 24 (univregs-stats)</h2>
@@ -1112,9 +1124,11 @@
                     <p><a href="https://sos1920-24.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataUnivregsStats}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllUnivregsStats}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container24"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-26" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 26 (goalscorers)</h2>
@@ -1129,9 +1143,11 @@
                     <p><a href="https://sos1920-26.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataGoalscorers}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllGoalscorers}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container26"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-28" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 28 (ppas)</h2>
@@ -1146,9 +1162,11 @@
                     <p><a href="https://sos1920-28.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataPpas}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllPpas}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container28"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-30" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API del Grupo 30 (indice_de_masa_corporal)</h2>
@@ -1163,9 +1181,11 @@
                     <p><a href="https://sos1920-30.herokuapp.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataIndiceMasaCorporal}">Cargar Datos Iniciales</Button></p>
                     <p><Button color="danger" on:click="{deleteAllIndiceMasaCorporal}">Elimina Todos los Recursos</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="container30"></div>
                     </figure>
+                </div>
                 </div>
                 <div class="tab-pane fade" id="list-ext1" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API Externa (COVID-19)</h2>
@@ -1179,9 +1199,11 @@
                     <p><a href="/"><Button color="info">Volver</Button></a></p>
                     <p><a href="https://covid19tracking.narrativa.com/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataEXT1}">Cargar Datos Iniciales</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="containerExt1"></div>
                     </figure>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="list-ext2" role="tabpanel" aria-labelledby="list-profile-list">
                     <h2>Uso de API Externa (OpenWeatherMap)</h2>
@@ -1195,9 +1217,11 @@
                     <p><a href="/"><Button color="info">Volver</Button></a></p>
                     <p><a href="https://home.openweathermap.org/"><Button color="primary">Página Web</Button></a></p>
                     <p><Button color="success" on:click="{loadInitialDataEXT2}">Cargar Datos Iniciales</Button></p>
+                    <div class="container">
                     <figure class="highcharts-figure">
                         <div id="containerExt2"></div>
                     </figure>
+                    </div>
                 </div>
             </div>
         </div>
