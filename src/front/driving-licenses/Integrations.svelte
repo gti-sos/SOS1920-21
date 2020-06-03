10692bae331acc95ad09b606e275514c7f23ca45
unknown <antonio98pt@gmail.com>
2020-06-03 13:46:07 +0200

    5 integraciones terminadas

--- src/front/driving-licenses/Integrations.svelte
+++ src/front/driving-licenses/Integrations.svelte
@@ -37,8 +37,132 @@
     });
   }
 
+  async function loadInitialOffworksStats() {
+    const res = await fetch(
+      "https://sos1920-23.herokuapp.com/api/v2/offworks-stats/loadInitialData",
+      {
+        method: "GET"
+      }
+    ).then(function(res) {
+      highchartsGraphG23();
+      infoAlertStatus = res.status + " - " + res.statusText;
+      infoAlertText = "Recursos cargados correctamente.";
+    });
+  }
+
+  async function deleteAllOffworksStats() {
+    const res = await fetch(
+      "https://sos1920-23.herokuapp.com/api/v2/offworks-stats",
+      {
+        method: "DELETE"
+      }
+    ).then(function(res) {
+      if (res.status == 404) {
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "No hay recursos que eliminar.";
+      } else {
+        highchartsGraphG23();
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "Se han eliminado todos los recursos correctamente.";
+      }
+    });
+  }
+
+  async function loadInitialDataFormulaStats() {
+    const res = await fetch(
+      "https://sos1920-22.herokuapp.com/api/v2/formula-stats/loadInitialData",
+      {
+        method: "GET"
+      }
+    ).then(function(res) {
+      highchartsGraphG1();
+      infoAlertStatus = res.status + " - " + res.statusText;
+      infoAlertText = "Recursos cargados correctamente.";
+    });
+  }
+
+  async function deleteAllFormulaStats() {
+    const res = await fetch(
+      "https://sos1920-22.herokuapp.com/api/v2/formula-stats",
+      {
+        method: "DELETE"
+      }
+    ).then(function(res) {
+      if (res.status == 404) {
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "No hay recursos que eliminar.";
+      } else {
+        highchartsGraphG23();
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "Se han eliminado todos los recursos correctamente.";
+      }
+    });
+  }
+
+  async function loadInitialDataUclStats() {
+    const res = await fetch(
+      "https://sos1920-08.herokuapp.com/api/v1/ucl_stats/loadInitialData",
+      {
+        method: "GET"
+      }
+    ).then(function(res) {
+      highchartsGraphG8();
+      infoAlertStatus = res.status + " - " + res.statusText;
+      infoAlertText = "Recursos cargados correctamente.";
+    });
+  }
+
+  async function deleteAllUclStats() {
+    const res = await fetch(
+      "https://sos1920-08.herokuapp.com/api/v1/ucl_stats",
+      {
+        method: "DELETE"
+      }
+    ).then(function(res) {
+      if (res.status == 404) {
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "No hay recursos que eliminar.";
+      } else {
+        highchartsGraphG8();
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "Se han eliminado todos los recursos correctamente.";
+      }
+    });
+  }
+
+  async function loadInitialGlobalTransfers() {
+    const res = await fetch(
+      "https://sos1920-26.herokuapp.com/api/v2/global-transfers/loadInitialData",
+      {
+        method: "GET"
+      }
+    ).then(function(res) {
+      highchartsGraphG26();
+      infoAlertStatus = res.status + " - " + res.statusText;
+      infoAlertText = "Recursos cargados correctamente.";
+    });
+  }
+
+  async function deleteAllGlobalTransfers() {
+    const res = await fetch(
+      "https://sos1920-26.herokuapp.com/api/v2/global-transfers",
+      {
+        method: "DELETE"
+      }
+    ).then(function(res) {
+      if (res.status == 404) {
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "No hay recursos que eliminar.";
+      } else {
+        highchartsGraphG26();
+        infoAlertStatus = res.status + " - " + res.statusText;
+        infoAlertText = "Se han eliminado todos los recursos correctamente.";
+      }
+    });
+  }
+
   async function highchartsGraphG1() {
-    console.log("Fetching poverty-stats...");
+    console.log("Fetching Natality-stats...");
     const data = await fetch(
       "https://sos1920-01.herokuapp.com/api/v2/natality-stats"
     );
@@ -99,29 +223,25 @@
   }
   highchartsGraphG1();
 
-  async function highchartsGraphG2() {
-    console.log("Fetching Natality-stats...");
+  async function highchartsGraphG8() {
+    console.log("Fetching ucl_stats...");
     const data = await fetch(
-      "https://sos1920-01.herokuapp.com/api/v2/natality-stats"
+      "https://sos1920-08.herokuapp.com/api/v1/ucl_stats"
     );
     let jsonData = await data.json();
     console.log(jsonData);
-    var natalityData = jsonData
+    var uclData = jsonData
       .filter(function(x) {
-        return x.country && parseInt(x.year) == 2010;
+        return x.country && x.team;
       })
       .map(dato => {
         return {
-          name: dato.country,
-          y: [
-            parseInt(dato.natality_men),
-            parseInt(dato.natality_women),
-            parseInt(dato.natality_totals)
-          ]
+          name: dato.country + " - " + dato.team,
+          y: parseInt(dato.title)
         };
       });
 
-    Highcharts.chart("container2", {
+    Highcharts.chart("container8", {
       chart: {
         plotBackgroundColor: null,
         plotBorderWidth: null,
@@ -129,10 +249,10 @@
         type: "pie"
       },
       title: {
-        text: "Natalidad por sexos"
+        text: "Porcentaje de títulos europeos ganados"
       },
       subtitle: {
-        text: "(2010)"
+        text: "(Hasta 2019)"
       },
       tooltip: {
         pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
@@ -152,14 +272,195 @@
           showInLegend: true
         }
       },
+      series: [
+        {
+          name: "Porcentaje",
+          colorByPoint: true,
+          data: uclData
+        }
+      ]
+    });
+  }
+  highchartsGraphG8();
+
+  async function highchartsGraphG23() {
+    console.log("Fetching offworks-stats...");
+    const data = await fetch(
+      "https://sos1920-23.herokuapp.com/api/v2/offworks-stats"
+    );
+    let jsonData = await data.json();
+    console.log(jsonData);
+
+    var offworksData = jsonData
+      .filter(function(x) {
+        return x.community && parseInt(x.year) == 2008;
+      })
+      .map(dato => {
+        return {
+          name: dato.community + " - " + dato.year,
+          data: [
+            parseInt(dato.accident),
+            parseInt(dato.sick),
+            parseInt(dato.numberzone)
+          ]
+        };
+      });
+
+    Highcharts.chart("container23", {
+      chart: {
+        type: "area"
+      },
+      title: {
+        text: "Accidentes laborales"
+      },
+      subtitle: {
+        text: "(2008)"
+      },
+      xAxis: {
+        categories: [
+          "Baja por accidente",
+          "Baja por enfermedad",
+          "Número de zona"
+        ],
+        tickmarkPlacement: "on",
+        title: {
+          enabled: false
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Números"
+        }
+      },
+      plotOptions: {
+        area: {
+          stacking: "normal",
+          lineColor: "#666666",
+          lineWidth: 1,
+          marker: {
+            lineWidth: 1,
+            lineColor: "#666666"
+          }
+        }
+      },
+      series: offworksData
+    });
+  }
+  highchartsGraphG23();
+
+  async function highchartsGraphG22() {
+    console.log("Fetching formula-stats...");
+    const data = await fetch(
+      "https://sos1920-22.herokuapp.com/api/v2/formula-stats"
+    );
+    let jsonData = await data.json();
+    console.log(jsonData);
+    var natalityData = jsonData
+      .filter(function(x) {
+        return x.country && parseInt(x.year);
+      })
+      .map(dato => {
+        return {
+          name: dato.country + "-" + parseInt(dato.year),
+          data: [
+            parseInt(dato.pilotnumber),
+            parseInt(dato.victorynumber),
+            parseInt(dato.totalpointnumber)
+          ]
+        };
+      });
+
+    Highcharts.chart("container22", {
+      chart: {
+        type: "column"
+      },
+      title: {
+        text: ""
+      },
+      xAxis: {
+        categories: [
+          "Número de pilotos",
+          "Número de Victorias",
+          "Número de puntos"
+        ],
+        crosshair: true
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: " "
+        }
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
       series: natalityData
     });
   }
-  highchartsGraphG2();
+  highchartsGraphG22();
+
+  async function highchartsGraphG26() {
+    console.log("Fetching global-transfers...");
+    const data = await fetch(
+      "https://sos1920-26.herokuapp.com/api/v2/global-transfers"
+    );
+    let jsonData = await data.json();
+    console.log(jsonData);
+    var globalTransfersData = jsonData
+      .filter(function(x) {
+        return x.team && parseInt(x.year);
+      })
+      .map(dato => {
+        return {
+          name: dato.team + " - " + parseInt(dato.year),
+          data: [parseInt(dato.signing), parseInt(dato.sale)]
+        };
+      });
+
+    Highcharts.chart("container26", {
+      chart: {
+        type: "column"
+      },
+      title: {
+        text: "Dinero gastado y obtenido en fichajes"
+      },
+      xAxis: {
+        categories: ["Fichajes", "Ventas"],
+        crosshair: true
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: "Millones de Euros"
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat:
+          '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} millones de Euros </b></td></tr>',
+        footerFormat: "</table>",
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: globalTransfersData
+    });
+  }
+  highchartsGraphG26();
 </script>
 
 <main>
-  <h2>Integraciones - Accidentes de Tráfico</h2>
+  <h2>Integraciones - Licencias de Conducir</h2>
   <br />
   <div class="row">
     <div class="col-4">
@@ -175,6 +476,50 @@
           Integración con Grupo 1
           <br />
           (Natality-stats)
+        </a>
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-profile-list"
+          data-toggle="list"
+          href="#list-02"
+          role="tab"
+          aria-controls="profile">
+          Integración con Grupo 8
+          <br />
+          (ucl_stats)
+        </a>
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-profile-list"
+          data-toggle="list"
+          href="#list-03"
+          role="tab"
+          aria-controls="profile">
+          Integración con Grupo 23
+          <br />
+          (offworks-stats)
+        </a>
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-profile-list"
+          data-toggle="list"
+          href="#list-04"
+          role="tab"
+          aria-controls="profile">
+          Integración con Grupo 22
+          <br />
+          (formula-stats)
+        </a>
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-profile-list"
+          data-toggle="list"
+          href="#list-05"
+          role="tab"
+          aria-controls="profile">
+          Integración con Grupo 26
+          <br />
+          (global-transfers)
         </a>
       </div>
     </div>
@@ -195,7 +540,12 @@
           {/if}
           <p>
             <a href="/">
-              <Button color="info">Volver a Inicio</Button>
+              <Button color="info">Volver</Button>
+            </a>
+          </p>
+          <p>
+            <a href="https://sos1920-01.herokuapp.com/">
+              <Button color="primary">Página Web</Button>
             </a>
           </p>
           <p>
@@ -210,6 +560,154 @@
           </p>
           <figure class="highcharts-figure">
             <div id="container1" />
+          </figure>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="list-02"
+          role="tabpanel"
+          aria-labelledby="list-home-list">
+          <h2>Integración con Grupo 8 (ucl_stats)</h2>
+          <h3>Acciones</h3>
+          {#if infoAlertStatus}
+            <Alert>
+              <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
+              {infoAlertText}
+            </Alert>
+          {/if}
+          <p>
+            <a href="/">
+              <Button color="info">Volver</Button>
+            </a>
+          </p>
+          <p>
+            <a href="https://sos1920-08.herokuapp.com/">
+              <Button color="primary">Página Web</Button>
+            </a>
+          </p>
+          <p>
+            <Button color="success" on:click={loadInitialDataUclStats}>
+              Cargar Datos Iniciales
+            </Button>
+          </p>
+          <p>
+            <Button color="danger" on:click={deleteAllUclStats}>
+              Elimina Todos los Recursos
+            </Button>
+          </p>
+          <figure class="highcharts-figure">
+            <div id="container8" />
+          </figure>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="list-03"
+          role="tabpanel"
+          aria-labelledby="list-profile-list">
+          <h2>Integración con Grupo 23 (offworks-stats)</h2>
+          <h3>Acciones</h3>
+          {#if infoAlertStatus}
+            <Alert>
+              <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
+              {infoAlertText}
+            </Alert>
+          {/if}
+          <p>
+            <a href="/">
+              <Button color="info">Volver</Button>
+            </a>
+          </p>
+          <p>
+            <a href="https://sos1920-23.herokuapp.com/">
+              <Button color="primary">Página Web</Button>
+            </a>
+          </p>
+          <p>
+            <Button color="success" on:click={loadInitialOffworksStats}>
+              Cargar Datos Iniciales
+            </Button>
+          </p>
+          <p>
+            <Button color="danger" on:click={deleteAllOffworksStats}>
+              Elimina Todos los Recursos
+            </Button>
+          </p>
+          <figure class="highcharts-figure">
+            <div id="container23" />
+          </figure>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="list-04"
+          role="tabpanel"
+          aria-labelledby="list-profile-list">
+          <h2>Integración con Grupo 22 (formula-stats)</h2>
+          <h3>Acciones</h3>
+          {#if infoAlertStatus}
+            <Alert>
+              <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
+              {infoAlertText}
+            </Alert>
+          {/if}
+          <p>
+            <a href="/">
+              <Button color="info">Volver</Button>
+            </a>
+          </p>
+          <p>
+            <a href="https://sos1920-22.herokuapp.com/">
+              <Button color="primary">Página Web</Button>
+            </a>
+          </p>
+          <p>
+            <Button color="success" on:click={loadInitialDataFormulaStats}>
+              Cargar Datos Iniciales
+            </Button>
+          </p>
+          <p>
+            <Button color="danger" on:click={deleteAllFormulaStats}>
+              Elimina Todos los Recursos
+            </Button>
+          </p>
+          <figure class="highcharts-figure">
+            <div id="container22" />
+          </figure>
+        </div>
+        <div
+          class="tab-pane fade"
+          id="list-05"
+          role="tabpanel"
+          aria-labelledby="list-profile-list">
+          <h2>Integración con Grupo 26 (global-transfers)</h2>
+          <h3>Acciones</h3>
+          {#if infoAlertStatus}
+            <Alert>
+              <h4 class="alert-heading text-capitalize">{infoAlertStatus}</h4>
+              {infoAlertText}
+            </Alert>
+          {/if}
+          <p>
+            <a href="/">
+              <Button color="info">Volver</Button>
+            </a>
+          </p>
+          <p>
+            <a href="https://sos1920-26.herokuapp.com/">
+              <Button color="primary">Página Web</Button>
+            </a>
+          </p>
+          <p>
+            <Button color="success" on:click={loadInitialGlobalTransfers}>
+              Cargar Datos Iniciales
+            </Button>
+          </p>
+          <p>
+            <Button color="danger" on:click={deleteAllGlobalTransfers}>
+              Elimina Todos los Recursos
+            </Button>
+          </p>
+          <figure class="highcharts-figure">
+            <div id="container26" />
           </figure>
         </div>
       </div>
