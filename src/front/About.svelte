9f152c77fb8e372f4cd1a533a24a95738ec02c8a
unknown <antonio98pt@gmail.com>
2020-06-04 07:57:00 +0200

    Vídeo subido

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -9,6 +9,6 @@
     <h3>Licencias de Conducir <span class="badge badge-primary">driving-licenses</span></h3>
     <div class="embed-responsive embed-responsive-16by9">
         <iframe title="driving-licenses" class="embed-responsive-item"
-            src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+            src="https://youtu.be/A6IzdsaN23I" allowfullscreen></iframe>
     </div>
 </div>
